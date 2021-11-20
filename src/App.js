a94cd41ae793192e4335a1f9382e30b19c4a8019
Jackson Wold <jacksonrwold@gmail.com>
2021-11-20 15:05:07 -0600

    made the basic react app look a little nicer

--- src/App.js
+++ src/App.js
@@ -14,11 +14,12 @@ function App() {
         <div>Counter: {counter}</div>
         <button onClick={() => dispatch({type: "INCREMENT"})}>increment</button>
         <button onClick={() => dispatch({type: "DECREMENT"})}>decrement</button>
+      
+        <div className="foo-thing">
+          <div>Foo: { foo ? "true" : "false"}</div>
+          <button onClick={() => dispatch(thunkFunction())}>change foo</button>
+        </div>
       </header>
-      <div className="foo-thing">
-        <div>Foo: { foo ? "true" : "false"}</div>
-        <button onClick={() => dispatch(thunkFunction())}></button>
-      </div>
     </div>
   );
 }
